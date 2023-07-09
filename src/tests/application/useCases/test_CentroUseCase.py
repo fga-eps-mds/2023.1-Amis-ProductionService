f718e2b65de93b5574f7bfae2de4ce346df59d6e
FilhoDoan <doangalvaofilho@gmail.com>
2023-07-09 01:39:41 -0300

    Adicionando testes validos de centroUseCase
    Co-authored-by: Caio Lelis  <cayolelisdf@gmail.com>

--- src/tests/application/useCases/test_CentroUseCase.py
+++ src/tests/application/useCases/test_CentroUseCase.py
@@ -1,10 +1,11 @@
 from application.useCases.CentroUseCase import CentroUseCase
 from infrastructure.repositories.CentroRepository import CentroRepository
 from infrastructure.repositories.CentroInscricoesRepository import CentroInscricoesRepository
-from domain.entities.Centro import Centro
+from domain.entities.Centro import Centro, CentroResponse, CentroRequest
 from unittest.mock import MagicMock
 import pytest
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from typing import NoReturn
 
 test_session = UnifiedAlchemyMagicMock
 
@@ -13,27 +14,29 @@ def get_mock_db_session():
 
 centro_repo = CentroRepository(session = get_mock_db_session())
 centro_inscricoes = CentroInscricoesRepository(session=get_mock_db_session())
-centro_useCase = CentroUseCase(
+centro_use_Case = CentroUseCase(
     centroRepository=centro_repo,
     centroInscricoesRepository = centro_inscricoes,
 )
 
-test_listCentro = [ 
-    Centro (
-        descricao= "Centro produtivo de teste 2/3",
-        data_agendada = "2023-07-08",
-        status = 1,
-        turno = 1,
-        vagas = 23
-        ),
-    Centro(
-        descricao= "Centro produtivo de teste ",
-        data_agendada = "2023-07-07",
-        status = 1,
-        turno = 2,
-        vagas = 28
-    ),
-]
+test_listCentro= [ 
+    (Centro (
+    descricao= "Centro produtivo de teste 2/3",
+       data_agendada= "2023-07-08",
+       status= 1,
+       turno= 1,
+       vagas= 21,
+       id= 2,
+       )),
+    (Centro(
+        descricao= "Centro produtivo de teste",
+        data_agendada= "2023-07-07",
+        status= 1,
+        turno= 2,
+        vagas= 28,
+        id= 3 
+    )),
+] 
 
 @pytest.mark.parametrize(
     "centroSent",
@@ -41,6 +44,9 @@ test_listCentro = [
 )
 
 def test_save_centro(centroSent):
-    response = centro_useCase.save(centroSent= centroSent)
+    response = centro_use_Case.save(centroSent= centroSent)
     assert centroSent == response, response.__dict__
+
+
+
 
